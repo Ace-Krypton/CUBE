cd2454949a4095685df6f4f9bd286df38f0a22f4
Ace-Krypton <ramizna@code.edu.az>
2022-11-24 21:14:53 +0100

    Added `TODO list`

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+/* TODO List
+ * 1. Distro Display
+ * 2. Linux version
+ * 3. Packages
+ * 4. Shell version
+ * 5. Up Time
+ * 6. Storage
+ * 7. RAM percentage */
+
 auto main() -> int {
-    std::cout << "This will be cubefetch" << std::endl;
+
     return 0;
 }
