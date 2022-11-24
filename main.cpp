4f652248edc7bbb8abdca25201f94d30c0b96055
Ace-Krypton <ramizna@code.edu.az>
2022-11-24 22:05:50 +0100

    Added `CPU info`

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 /* TODO List
  * 1. Distro Display
@@ -9,7 +10,25 @@
  * 6. Storage
  * 7. RAM percentage */
 
-auto main() -> int {
+auto get_cpu() -> std::string {
+    std::string model_name { "model name" }, cpu_info { };
+    std::ifstream file("/proc/cpuinfo");
 
-    return 0;
+    if (!(file.is_open())) return { };
+
+    for (std::string line; (std::getline(file,line));) {
+        if (line.find(model_name) != std::string::npos) {
+            const std::size_t start_pos = line.find(model_name);
+            std::string temp = line.substr(start_pos+6);
+            const std::size_t stop_pos = temp.find('\"');
+            std::string result = temp.substr(0, stop_pos);
+            std::cout << result << std::endl;
+        }
+    }
+    return cpu_info;
+}
+
+auto main() -> int {
+    get_cpu();
+    return 0x0;
 }
